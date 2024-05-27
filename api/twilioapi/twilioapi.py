6421f54ebf7b01b8ad6f1656d5d2e9816a73c0e5
root <root@inst-builder-debian-11-build-build-tlb04.us-central1-a.c.gce-image-builder.internal>
2024-05-27 12:22:00 +0000

    some enchancements

--- api/twilioapi/twilioapi.py
+++ api/twilioapi/twilioapi.py
@@ -1,21 +1,13 @@
-from fastapi import APIRouter
-from fastapi import Form
-from fastapi import Response
-from aiohttp import ClientSession
-from aiohttp import BasicAuth
-
+from fastapi import APIRouter, Form, Response
+from aiohttp import ClientSession, BasicAuth
 from utils.config import config
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
 tw_router = APIRouter()
 NUMBER_TO_REDIRECT = "+380989602010"
 
-
 async def set_webhook() -> None:
-    url = "https://api.twilio.com/2010-04-01/Accounts/{}/IncomingPhoneNumbers/{}.json".format(
-        config.ACCOUNT_SID,
-        config.PHONE_NUMBER_SID
-    )
+    url = f"https://api.twilio.com/2010-04-01/Accounts/{config.ACCOUNT_SID}/IncomingPhoneNumbers/{config.PHONE_NUMBER_SID}.json"
     data = {
         'VoiceUrl': f'{config.PUBLIC_URL}/api/twilio/voice'
     }
@@ -25,29 +17,38 @@ async def set_webhook() -> None:
             if response.status >= 300:
                 raise Exception(f"Failed to set webhook. {response_data}")
 
-
-@tw_router.get("/voice")
+@tw_router.post("/voice")
 async def incoming():
     response = VoiceResponse()
-    response.say("Hello! I am AI helper. Please, say who you are and provide a reason why are you calling mrs. Smith")
-    response.gather(input='speech', action=f'{config.PUBLIC_URL}/api/twilio/gather', timeout=5)
-    return Response(content=str(response), headers={"Content-Type": "text/xml"})
-
+    gather = Gather(input='speech', action=f'{config.PUBLIC_URL}/api/twilio/gather', timeout=5)
+    gather.say("Hello! I am AI helper. Please, say who you are and provide a reason why you are calling Mrs. Smith.")
+    response.append(gather)
+    response.redirect(f'{config.PUBLIC_URL}/api/twilio/voice')
+    return Response(content=str(response), media_type="application/xml")
 
 @tw_router.post("/gather")
 async def gather(SpeechResult: str = Form(), CallSid: str = Form()) -> Response:
-    print(SpeechResult)
+    print("Speech Result:", SpeechResult)
     response = VoiceResponse()
-    # feeding speech result to ai model
-    ai_speech_response = "Okay, I got you. Provide more details"
-    if "ACCEPT" in ai_speech_response:
-        response.say("Thank you. You have passed the control. Redirecting to the target user")
-        # response.dial(number=NUMBER_TO_REDIRECT)
-        response.hangup()
-    elif "DECLINE" in ai_speech_response:
-        response.say("You have not passed the control. Good bye")
-        response.hangup()
+    # feeding speech result to AI model
+    ai_speech_response = "Okay, I got you. Provide more details."
+    if SpeechResult:
+        # If speech was recognized, process the input
+        if "ACCEPT" in ai_speech_response:
+            response.say("Thank you. You have passed the control. Redirecting to the target user.")
+            response.dial(NUMBER_TO_REDIRECT)
+        elif "DECLINE" in ai_speech_response:
+            response.say("You have not passed the control. Goodbye.")
+            response.hangup()
+        else:
+            response.say(ai_speech_response)
+            gather = Gather(input='speech', action=f'{config.PUBLIC_URL}/api/twilio/gather', timeout=5)
+            response.append(gather)
     else:
-        response.say(ai_speech_response)
-        response.gather(input='speech', action=f'{config.PUBLIC_URL}/api/twilio/gather', timeout=5)
-    return Response(content=str(response), headers={"Content-Type": "text/xml"})
+        # If no speech was recognized, prompt the user to try again
+        response.say("I'm sorry, I didn't catch that. Please try again.")
+        gather = Gather(input='speech', action=f'{config.PUBLIC_URL}/api/twilio/gather', timeout=5)
+        response.append(gather)
+    return Response(content=str(response), media_type="application/xml")
+
+
